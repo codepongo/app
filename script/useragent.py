7610baefd3f48fa2da6c4eab1ed70336d840e6a4
unknown <zhuhuotui@gmail.com>
2014-12-15 11:24:53 +0800

    delete the debug information

--- script/useragent.py
+++ script/useragent.py
@@ -4,6 +4,5 @@ sys.path.append(os.path.dirname(__file__))
 import web
 class useragent:
     def GET(self):
-        print 'hello'
         render = web.template.render(os.path.join(os.path.dirname(__file__), 'templates'))
         return render.whatsmyuseragent(web.ctx.env.get('HTTP_USER_AGENT'))

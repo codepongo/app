285a2f9918700b9abcea33299e96ede75ecaef37
unknown <zhuhuotui@gmail.com>
2016-05-17 15:31:29 +0800

    mobile view and app

--- script/app.py
+++ script/app.py
@@ -39,7 +39,6 @@ class index:
                 'zshellext',
                 'zjruler',
                 'bjfoodprice',
-                'cnweatherforecast',
         ]
         briefs = []
         path = os.path.join(os.path.dirname(__file__), 'storage')

439a60e11e1c065959118d4a37c552764bb01366
unknown <zhuhuotui@gmail.com>
2016-09-23 11:22:16 +0800

    fix weather

--- script/weather.py
+++ script/weather.py
@@ -5,7 +5,7 @@ import weatherbroadcast
 class broadcast:
     def GET(self):
         render = web.template.render(os.path.join(os.path.dirname(__file__), 'templates'))
-        return render.weather(weatherbroadcast.baidu())
+        return render.weather(weatherbroadcast.baiduapi('beijing'))
     def POST(self):
         render = web.template.render(os.path.join(os.path.dirname(__file__), 'templates'))
-        return render.weather(weatherbroadcast.baidu(web.input().city.encode('utf8')))
+        return render.weather(weatherbroadcast.baiduapi(web.input().city.encode('utf8')))
